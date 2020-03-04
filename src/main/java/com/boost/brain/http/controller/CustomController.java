a4cc41617719402fe2c386281e7f88287ab78641
BliznyukS <bliznyuk.sergey@gmail.com>
2020-03-04 16:35:28 +0200

    CustomController added GetMapping "/"

--- src/main/java/com/boost/brain/http/controller/CustomController.java
+++ src/main/java/com/boost/brain/http/controller/CustomController.java
@@ -1,12 +1,22 @@
 package com.boost.brain.http.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 public class CustomController {
-    @RequestMapping(value = "/custom", method = RequestMethod.GET)
+
+    @GetMapping(path = "/")
+    public void swaggerUI(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
+
+    @RequestMapping(value = "/custom", method = RequestMethod.POST)
     public String custom() {
         return "custom";
     }
